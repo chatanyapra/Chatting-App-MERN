62d0ba7f786a57cc96cb3302285d221041652b33
chatanyapra <chatanyapra@gmail.com>
2024-06-29 12:41:17 +0530

    Signup complete

--- frontend/src/hooks/useSignup.ts
+++ frontend/src/hooks/useSignup.ts
@@ -41,6 +41,9 @@ const useSignup = () => {
             });
 
             const data = await res.json();
+            if(data.error){
+                throw new Error(data.error);
+            }
             console.log(data);
 
         } catch (error: any) {
