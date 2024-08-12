e6e44b8cc65059981e1332d72fbc0b6d79f40b98
chatanyapra <chatanyapra@gmail.com>
2024-08-12 23:12:18 +0530

    pp

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -65,6 +65,10 @@ io.on("connection", (socket) => {
         io.to(to).emit("peer:nego:final", { from: socket.id, ans });
     });
 
+    socket.on("call:accept:calley", ({ to }) => {
+        io.to(to).emit("call:accept:calley", { from: socket.id });
+    });
+
     socket.on('call:end', ({ to }) => {
         console.log("new disconnect--------------");
         socket.to(to).emit('call:end');
